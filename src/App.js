e456a4c18be5642fb8745004abe914be1bdbcedd
Nikita Varabei <nvarabei@gmail.com>
2021-11-06 05:33:19 -0400

    implement rpg game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,176 @@
-import React from 'react';
-import twitterLogo from './assets/twitter-logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import twitterLogo from './assets/twitter-logo.svg';
+import { ethers } from 'ethers';
+
+import SelectCharacter from './Components/SelectCharacter';
+import Arena from './Components/Arena';
+import LoadingIndicator from './Components/LoadingIndicator';
+import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
+import myEpicGame from './utils/MyEpicGame.json';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  // State
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  /*
+  * Right under current account, setup this new state property
+  */
+  const [characterNFT, setCharacterNFT] = useState(null);
+  /*
+* New state property added here
+*/
+const [isLoading, setIsLoading] = useState(false);
+
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        /*
+         * We set isLoading here because we use return in the next line
+         */
+        setIsLoading(false);
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    /*
+     * We release the state property after all the function logic
+     */
+    setIsLoading(false);
+};
+
+  const renderContent = () => {
+    /*
+     * If the app is currently loading, just render out LoadingIndicator
+     */
+    if (isLoading) {
+      return <LoadingIndicator />;
+    }
+  
+    if (!currentAccount) {
+      return (
+        <div className="connect-wallet-container">
+             <img
+              src="https://media4.giphy.com/media/Xf8OScCaFkhyg5nxYn/giphy.gif?cid=ecf05e471rw2qswzbfl914te2fqu5dv5tf27iu72h9oze6i8&rid=giphy.gif&ct=g"
+              alt="Fantasy World"
+            />
+          <button
+            className="cta-button connect-wallet-button"
+            onClick={connectWalletAction}
+          >
+            Connect Wallet To Get Started
+          </button>
+        </div>
+      );
+    } else if (currentAccount && !characterNFT) {
+      return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    } else if (currentAccount && characterNFT) {
+      return (
+        <Arena characterNFT={characterNFT} setCharacterNFT={setCharacterNFT} />
+      );
+    }
+  };
+
+
+  /*
+   * Implement your connectWallet method here
+   */
+  const connectWalletAction = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        alert('Get MetaMask!');
+        return;
+      }
+
+      /*
+       * Fancy method to request access to account.
+       */
+      const accounts = await ethereum.request({
+        method: 'eth_requestAccounts',
+      });
+
+      /*
+       * Boom! This should print out public address once we authorize Metamask.
+       */
+      console.log('Connected', accounts[0]);
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+// UseEffects
+useEffect(() => {
+  /*
+   * Anytime our component mounts, make sure to immiediately set our loading state
+   */
+  setIsLoading(true);
+  checkIfWalletIsConnected();
+}, []);
+
+useEffect(() => {
+  const fetchNFTMetadata = async () => {
+    console.log('Checking for Character NFT on address:', currentAccount);
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+    const gameContract = new ethers.Contract(
+      CONTRACT_ADDRESS,
+      myEpicGame.abi,
+      signer
+    );
+
+    const characterNFT = await gameContract.checkIfUserHasNFT();
+    if (characterNFT.name) {
+      console.log('User has character NFT');
+      setCharacterNFT(transformCharacterData(characterNFT));
+    }
+
+    /*
+     * Once we are done with all the fetching, set loading state to false
+     */
+    setIsLoading(false);
+  };
+
+  if (currentAccount) {
+    console.log('CurrentAccount:', currentAccount);
+    fetchNFTMetadata();
+  }
+}, [currentAccount]);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
           <p className="header gradient-text">⚔️ Metaverse Slayer ⚔️</p>
           <p className="sub-text">Team up to protect the Metaverse!</p>
-          <div className="connect-wallet-container">
-            <img
-              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
-              alt="Monty Python Gif"
-            />
-          </div>
+          {/* This is where our button and image code used to be!
+          *	Remember we moved it into the render method.
+          */}
+          {renderContent()}
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
