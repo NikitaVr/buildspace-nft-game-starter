309add7c8313079f49feb9f8b57c76dcf9a918f0
Nikita Varabei <nVarabei@gmail.com>
2022-02-15 20:27:48 -0500

    update to newly deployed contract address

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,16 @@
-const CONTRACT_ADDRESS = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const CONTRACT_ADDRESS = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 
 /*
  * Add this method and make sure to export it on the bottom!
  */
 const transformCharacterData = (characterData) => {
-    return {
-      name: characterData.name,
-      imageURI: characterData.imageURI,
-      hp: characterData.hp.toNumber(),
-      maxHp: characterData.maxHp.toNumber(),
-      attackDamage: characterData.attackDamage.toNumber(),
-    };
+  return {
+    name: characterData.name,
+    imageURI: characterData.imageURI,
+    hp: characterData.hp.toNumber(),
+    maxHp: characterData.maxHp.toNumber(),
+    attackDamage: characterData.attackDamage.toNumber(),
   };
-  
-  export { CONTRACT_ADDRESS, transformCharacterData };
+};
+
+export { CONTRACT_ADDRESS, transformCharacterData };
