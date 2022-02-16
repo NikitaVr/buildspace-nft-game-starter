fe71e249a8e72d0be95d4d627e8b4f3a0faa03e3
Nikita Varabei <nVarabei@gmail.com>
2022-02-15 20:35:47 -0500

    use pinata gateway instead of cloudflare for ipfs

--- src/Components/Arena/index.js
+++ src/Components/Arena/index.js
@@ -1,52 +1,52 @@
-import React, { useEffect, useState } from 'react';
-import { ethers } from 'ethers';
-import { CONTRACT_ADDRESS, transformCharacterData } from '../../constants';
-import myEpicGame from '../../utils/MyEpicGame.json';
-import './Arena.css';
-import LoadingIndicator from '../LoadingIndicator';
+import React, { useEffect, useState } from "react";
+import { ethers } from "ethers";
+import { CONTRACT_ADDRESS, transformCharacterData } from "../../constants";
+import myEpicGame from "../../utils/MyEpicGame.json";
+import "./Arena.css";
+import LoadingIndicator from "../LoadingIndicator";
 
 /*
  * We pass in our characterNFT metadata so we can a cool card in our UI
  */
-const Arena = ({ characterNFT, setCharacterNFT  }) => {
+const Arena = ({ characterNFT, setCharacterNFT }) => {
   // State
   const [gameContract, setGameContract] = useState(null);
-    /*
-    * State that will hold our boss metadata
-    */
-    const [boss, setBoss] = useState(null);
-/*
-* We are going to use this to add a bit of fancy animations during attacks
-*/
-const [attackState, setAttackState] = useState('');
-/*
-* Toast state management
-*/
-const [showToast, setShowToast] = useState(false);
+  /*
+   * State that will hold our boss metadata
+   */
+  const [boss, setBoss] = useState(null);
+  /*
+   * We are going to use this to add a bit of fancy animations during attacks
+   */
+  const [attackState, setAttackState] = useState("");
+  /*
+   * Toast state management
+   */
+  const [showToast, setShowToast] = useState(false);
 
-const runAttackAction = async () => {
-  try {
-    if (gameContract) {
-      setAttackState('attacking');
-      console.log('Attacking boss...');
-      const attackTxn = await gameContract.attackBoss();
-      await attackTxn.wait();
-      console.log('attackTxn:', attackTxn);
-      setAttackState('hit');
+  const runAttackAction = async () => {
+    try {
+      if (gameContract) {
+        setAttackState("attacking");
+        console.log("Attacking boss...");
+        const attackTxn = await gameContract.attackBoss();
+        await attackTxn.wait();
+        console.log("attackTxn:", attackTxn);
+        setAttackState("hit");
 
         /*
-      * Set your toast state to true and then false 5 seconds later
-      */
+         * Set your toast state to true and then false 5 seconds later
+         */
         setShowToast(true);
         setTimeout(() => {
-            setShowToast(false);
+          setShowToast(false);
         }, 5000);
+      }
+    } catch (error) {
+      console.error("Error attacking boss:", error);
+      setAttackState("");
     }
-  } catch (error) {
-    console.error('Error attacking boss:', error);
-    setAttackState('');
-  }
-};
+  };
 
   // UseEffects
   useEffect(() => {
@@ -63,91 +63,93 @@ const runAttackAction = async () => {
 
       setGameContract(gameContract);
     } else {
-      console.log('Ethereum object not found');
+      console.log("Ethereum object not found");
     }
   }, []);
 
-  
-   // UseEffects
-   useEffect(() => {
+  // UseEffects
+  useEffect(() => {
     const fetchBoss = async () => {
-        const bossTxn = await gameContract.getBigBoss();
-        console.log('Boss:', bossTxn);
-        setBoss(transformCharacterData(bossTxn));
+      const bossTxn = await gameContract.getBigBoss();
+      console.log("Boss:", bossTxn);
+      setBoss(transformCharacterData(bossTxn));
     };
 
     /*
-    * Setup logic when this event is fired off
-    */
+     * Setup logic when this event is fired off
+     */
     const onAttackComplete = (newBossHp, newPlayerHp) => {
-        const bossHp = newBossHp.toNumber();
-        const playerHp = newPlayerHp.toNumber();
+      const bossHp = newBossHp.toNumber();
+      const playerHp = newPlayerHp.toNumber();
 
-        console.log(`AttackComplete: Boss Hp: ${bossHp} Player Hp: ${playerHp}`);
+      console.log(`AttackComplete: Boss Hp: ${bossHp} Player Hp: ${playerHp}`);
 
-        /*
-        * Update both player and boss Hp
-        */
-        setBoss((prevState) => {
-            return { ...prevState, hp: bossHp };
-        });
+      /*
+       * Update both player and boss Hp
+       */
+      setBoss((prevState) => {
+        return { ...prevState, hp: bossHp };
+      });
 
-        setCharacterNFT((prevState) => {
-            return { ...prevState, hp: playerHp };
-        });
+      setCharacterNFT((prevState) => {
+        return { ...prevState, hp: playerHp };
+      });
     };
 
     if (gameContract) {
-        fetchBoss();
-        gameContract.on('AttackComplete', onAttackComplete);
+      fetchBoss();
+      gameContract.on("AttackComplete", onAttackComplete);
     }
 
     /*
-    * Make sure to clean up this event when this component is removed
-    */
+     * Make sure to clean up this event when this component is removed
+     */
     return () => {
-        if (gameContract) {
-            gameContract.off('AttackComplete', onAttackComplete);
-        }
-    }
-}, [gameContract]);
+      if (gameContract) {
+        gameContract.off("AttackComplete", onAttackComplete);
+      }
+    };
+  }, [gameContract]);
 
-return (
+  return (
     <div className="arena-container">
-    {/* Add your toast HTML right here */}
-    {boss && characterNFT && (
-      <div id="toast" className={showToast ? 'show' : ''}>
-        <div id="desc">{`💥 ${boss.name} was hit for ${characterNFT.attackDamage}!`}</div>
-      </div>
-    )}
+      {/* Add your toast HTML right here */}
+      {boss && characterNFT && (
+        <div id="toast" className={showToast ? "show" : ""}>
+          <div id="desc">{`💥 ${boss.name} was hit for ${characterNFT.attackDamage}!`}</div>
+        </div>
+      )}
       {/* Boss */}
       {boss && (
         <div className="boss-container">
-        <div className={`boss-content  ${attackState}`}>
+          <div className={`boss-content  ${attackState}`}>
             <h2>🔥 {boss.name} 🔥</h2>
             <div className="image-content">
-            <img src={"https://cloudflare-ipfs.com/ipfs/" + boss.imageURI} alt={`Boss ${boss.name}`} />
-            <div className="health-bar">
+              <img
+                src={"https://gateway.pinata.cloud/ipfs/" + boss.imageURI}
+                alt={`Boss ${boss.name}`}
+              />
+              <div className="health-bar">
                 <progress value={boss.hp} max={boss.maxHp} />
                 <p>{`${boss.hp} / ${boss.maxHp} HP`}</p>
+              </div>
             </div>
-            </div>
-        </div>
-        <div className="attack-container">
+          </div>
+          <div className="attack-container">
             <button className="cta-button" onClick={runAttackAction}>
-            {`💥 Attack ${boss.name}`}
+              {`💥 Attack ${boss.name}`}
             </button>
-        </div>
-        {/* Add this right under your attack button */}
-        {attackState === 'attacking' && (
+          </div>
+          {/* Add this right under your attack button */}
+          {attackState === "attacking" && (
             <div className="loading-indicator">
-            <LoadingIndicator />
-            <p>Attacking ⚔️</p>
+              <LoadingIndicator />
+              <p>Attacking ⚔️</p>
             </div>
-        )}
+          )}
         </div>
-    )}
-  
+      )}
+
       {/* Replace your Character UI with this */}
       {characterNFT && (
         <div className="players-container">
@@ -157,7 +159,9 @@ return (
               <div className="image-content">
                 <h2>{characterNFT.name}</h2>
                 <img
-                  src={"https://cloudflare-ipfs.com/ipfs/" + characterNFT.imageURI}
+                  src={
+                    "https://gateway.pinata.cloud/ipfs/" + characterNFT.imageURI
+                  }
                   alt={`Character ${characterNFT.name}`}
                 />
                 <div className="health-bar">
